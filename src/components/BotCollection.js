158f4d12485b63452810284f0fe1cdd58819546e
JAMES <jameskibunja17@gmail.com>
2023-02-10 12:13:10 +0300

    Challenge Done

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,17 @@
-import React from "react";
 
-function BotCollection() {
+
+import React from "react";
+import BotCard from "./BotCard";
+
+function BotCollection({ collection, clickHandler, handleDelete }) {
   // Your code here
   return (
     <div className="ui four column grid">
+      {"Bot Collection click on a bot to add it to your army."}
       <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
+        {collection.map((bot) => (
+          <BotCard key={bot.id} bot={bot} clickHandler={clickHandler} handleDelete={handleDelete} />
+        ))}
       </div>
     </div>
   );
